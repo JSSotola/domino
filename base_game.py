3fa3d082ba59c783a05cfe16c9105f38e36e8d1b
Jirka Balhar <balhar.j@gmail.com>
2018-01-01 17:01:14 +0100

    turn functions + game class

--- base_game.py
+++ base_game.py
@@ -1,13 +1,33 @@
 from itertools import combinations_with_replacement
 from random import shuffle
 
+class Game:
+    def __init__(self):
+        self.table = []
+
+    def deal(self, player_list):
+        all_pieces = list(combinations_with_replacement(range(10), 2))
+        shuffle(all_pieces)
+        pieces_per_player = len(all_pieces)//len(player_list)
+        for player in player_list:
+            player.hand = all_pieces[:pieces_per_player]
+            all_pieces = all_pieces[pieces_per_player:]
+        self.table = [all_pieces[0]]
+
+    def play(self):
+        while 0 < min(len(player.hand) for player in list_players): #todo: all passed:
+            for player in list_players:
+                player.take_turn(self.table)
+
+                print(self.table)
 
 class Player:
     def __init__(self):
         self.hand = []
 
     def take_turn(self, game_state):
-        play(self.hand[0])
+        self.play(self.hand[0])
+        return True
 
     def play(self, piece):
         if game_state[0][0] in piece:
@@ -17,29 +37,14 @@ class Player:
 
         self.hand.remove(piece)
 
-def deal_game(player_list):
-    global game_state
-    all_pieces = list(combinations_with_replacement(range(10), 2))
-    shuffle(all_pieces)
-    pieces_per_player = len(all_pieces)//len(player_list)
-    for player in player_list:
-        player.hand = all_pieces[:pieces_per_player]
-        all_pieces = all_pieces[pieces_per_player:]
-    game_state = [all_pieces[0]]
-
 def create_players(number_of_players):
     l = []
     for i in range(number_of_players):
         l.append(Player())
     return l
 
-
-
-
 if __name__ == "__main__":
     list_players = create_players(4)
-    deal_game(list_players)
-    while 0 < min(len(player.hand) for player in list_players):
-        for player in list_players:
-            player.take_turn(game_state)
-            print(game_state)
+    game = Game()
+    game.deal(list_players)
+    game.play()
---
+++ players.py
@@ -0,0 +1,47 @@
+import random
+
+# class Player:
+# 	def __init__(self, hand, _turn):
+# 		self.hand = hand
+# 		self._turn = _turn
+
+# 	def turn(self, table):
+# 		return self._turn(self, table)
+
+def possible_turns(hand, first, last):
+	possible = []
+	for piece in hand:
+		if piece[0] == first or piece[1] == last:
+			possible.append(piece)
+
+	return possible
+
+# hraje náhodně
+def random_turn(self, table):
+	firstNum = table[0][0]
+	lastNum = table[-1][1]
+	possible = possible_turns(self.hand, firstNum, lastNum)
+	return random.choice(possible)
+
+# hraje vždy s největším součtem puntíků
+def max_turn(self, table):
+	firstNum = table[0][0]
+	lastNum = table[-1][1]
+	possible = possible_turns(self.hand, firstNum, lastNum)
+	possible = sorted(possible, key=lambda x: x[0]+x[1], reverse=True)
+	return possible[0]
+
+# snaží se pokládat co nejmenší koncový kámen
+def min_turn(self, table):
+	firstNum = max(table[0][0], table[-1][1])
+	lastNum = min(table[0][0], table[-1][1])
+	possible = possible_turns(self.hand, firstNum, lastNum)
+
+
+	# possible = sorted(possible, key=lambda x: 1 if x[0]==firstNum or x[1]==firstNum else 0, reverse=True)
+	return possible[0]
+
+
+# player = Player([(1,2), (1,3), (4,5)], max_turn)
+# print(player.turn([(1,5), (5,5)]))
+
